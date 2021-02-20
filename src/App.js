a9fcd7934ea4054aaf92d3bbcf1d748fcaf2b99e
Wilson Felipe <wfelipe2011@gmail.com>
2021-02-19 21:19:49 -0300

    Adicinando Cabecalho e Rodape

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
 import { validarCPF, validarSenha } from "./models/cadastro";
 import Cabecalho from './StyledComponentes/Cabecalho';
 import Footer from "./StyledComponentes/Footer";
-import Titulo from "./StyledComponentes/Titulo"
+
 
 function App() {
  
--- src/StyledComponentes/Container/index.jsx
+++ src/StyledComponentes/Container/index.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-  margin-top: 10%;
+  margin-top: 5%;
   background-color: ${({ theme }) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
